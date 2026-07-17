f213cfbb8b38d94f9399aae3d772f5e52e1e84e3
avishekdas2018 <dasavishek2013@gmail.com>
2026-07-17 09:14:34 +0530

    feat: add Clerk authentication dependencies and middleware for protected routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google"
-
 import "./globals.css"
+import { Geist, Geist_Mono } from "next/font/google"
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/sonner"
 import { cn } from "@/lib/utils"
+import { shadcn } from "@clerk/ui/themes"
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
@@ -29,10 +36,25 @@ export default function RootLayout({
       )}
     >
       <body>
-        <ThemeProvider>
-          {children}
-          <Toaster />
-        </ThemeProvider>
+        <ClerkProvider appearance={{ theme: shadcn }}>
+          <ThemeProvider>
+            <header className="flex h-16 items-center justify-end gap-4 p-4">
+              <Show when="signed-out">
+                <SignInButton />
+                <SignUpButton>
+                  <button className="h-10 cursor-pointer rounded-full bg-[#6c47ff] px-4 text-sm font-medium text-white sm:h-12 sm:px-5 sm:text-base">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </Show>
+              <Show when="signed-in">
+                <UserButton />
+              </Show>
+            </header>
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
   )
